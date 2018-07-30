8aeb3ad6497e4249e99a215732826e50ad9aef0b
Lorenz Diener <lorenzd@gmail.com>
2018-07-31 00:10:42 +0200

    soon.

---
+++ quasibot_mastoonly.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+import sys
+import random
+import hashlib
+import os
+
+import subprocess
+from subprocess import call
+
+from multiprocessing import Process, Queue
+
+from mastodon import Mastodon
+
+mastodon_api = Mastodon(
+    access_token = "mastodon_user.secret"
+)
+
+wordfile = open("wordlist.txt", "r")
+wordlist = wordfile.readlines()
+wordfile.close()
+
+# Thread handler that adds users that @ the bot to a queue
+replyQueue = Queue()
+def getReplies():
+    lastIdMastodon = None
+    while(True):
+        try:
+            replies = None
+            if lastIdMastodon == None:
+                replies = mastodon_api.timeline("mentions")
+            else:
+                print("Mastodon: Fetching replies since " + str(lastIdMastodon))
+                replies = mastodon_api.timeline("mentions", since_id = lastIdMastodon)
+
+            if len(replies) > 0:
+                lastIdMastodon= replies[0]["id"]
+                replies.reverse()
+
+            for reply in replies:
+                replyQueue.put((reply["account"]["acct"], "mastodon"))
+                print("Mastodon: New entry to reply queue: " + str(reply["account"]["acct"]))
+        except:
+            print("Mastodon: Error in fetch replies")
+            time.sleep(60 * 5)
+
+        time.sleep(60 * 5)
+
+Process(target = getReplies, daemon = True).start()
+
+servedUsers = []
+nextSeeds = []
+
+# Clear user queue once on startup
+time.sleep(5)
+debugExclude = []
+while(not replyQueue.empty()):
+   servedUser = replyQueue.get()
+   if not servedUser in debugExclude:
+       servedUsers.append(servedUser)
+   else:
+       print("Excluding " + str(servedUser) + " from initial queue wipe due to debug exclude")
+       nextSeeds.append(servedUser)
+
+startTime = 0
+pauseTime = 60 * 60 * 3
+while(True):
+    while(len(nextSeeds) == 0 or (time.time() - startTime > pauseTime)):
+        print("Checking new work...")
+        continueQueueGet = True
+        while continueQueueGet:
+            try:
+                print("Polling user queues.")
+                user = replyQueue.get(False)
+            except:
+                continueQueueGet = False
+                break
+            if user != None:
+                if not user in servedUsers and not user in nextSeeds:
+                    print("Appending " + str(user) + " to work queue.")
+                    nextSeeds.append(user)
+                    servedUsers.append(user)
+                if(len(servedUsers) > 300):
+                    servedUsers = servedUsers[len(servedUsers) - 300:]
+            else:
+                continueQueueGet = False
+        if(time.time() - startTime > pauseTime):
+            print("Time for seed post, prepending to work queue...")
+            startTime = time.time()
+            nextSeeds = [("___GEN___", "both")] + nextSeeds
+            
+        print("Sleeping until next check.")
+        time.sleep(60)
+    print("Current queue: " + str(nextSeeds))
+
+    seeduser = nextSeeds.pop(0)
+    seedphrase = seeduser[0]
+    userSpec = True
+    if(seedphrase == "___GEN___"):
+        seedphrase = ""
+        userSpec = False
+        for i in range(0, 5):
+                seedphrase += random.choice(wordlist).rstrip() + " "
+
+    try:
+        seedhash = str(abs(int(hashlib.sha1(seedphrase.encode("utf-8")).hexdigest(), 16)) % 4294967294)
+        print("seedphrase (no hash): " + seedphrase)
+        subprocess.call(["python3", "genquasi.py", seedhash])
+        subprocess.call(["ffmpeg", "-f", "lavfi", "-i", "anullsrc=channel_layout=stereo:sample_rate=44100", "-f", "gif", "-i", seedhash + ".gif", "-shortest", "-c:v", "libx264", "-b:v", "1M", "-pix_fmt", "yuv420p", "-c:a", "aac", seedhash + "_nl.mp4"])
+        
+        listfile = open(seedhash + ".txt", "w")
+        for i in range(0, 10):
+            listfile.write("file '" + seedhash + "_nl.mp4'\n")
+        listfile.close()
+        
+        subprocess.call(["ffmpeg", "-f", "concat", "-i", seedhash + ".txt", "-c:a", "copy", "-c:v", "copy", seedhash + ".mp4"])
+        subprocess.call(["mv", seedhash + ".gif", "done"])
+        subprocess.call(["mv", seedhash + ".mp4", "done"])
+        subprocess.call(["rm", seedhash + "_nl.mp4"])
+        subprocess.call(["rm", seedhash + ".txt"])
+        
+        mediaFile = "done/" + seedhash + ".gif"
+        if os.path.getsize(mediaFile) > 1024 * 1024 * 3.9:
+            mediaFile = "done/" + seedhash + ".mp4"
+    except:
+        print("Encountered error during encode. Link-only tweet.")
+        mediaFile = None
+    
+    if not os.path.exists(mediaFile):
+        mediaFile = None
+    
+
+    hqLink = "http://aka-san.halcy.de/quasi/" + seedhash + ".gif"
+    print("HQ link: " + hqLink)
+
+    # Post to Mastodon
+    try:
+        if seeduser[1] == "mastodon" or seeduser[1] == "both":
+            if mediaFile != None:
+                print("Mastodon media upload... mediafile is " + mediaFile)
+                mediaIdsMastodon = [mastodon_api.media_post(mediaFile)["id"]]
+            else:
+                mediaIdsMastodon = []
+            
+            print("Tooting...")
+            if userSpec == False:
+                mastodon_api.status_post("seed phrase: " + seedphrase + "(HQ: " + hqLink + " )", media_ids = mediaIdsMastodon)
+            else:
+                mastodon_api.status_post("@" + seedphrase + " here is your personal quasicrystal: (HQ: " + hqLink + " )", media_ids = mediaIdsMastodon)
+
+    except:
+        print("Encountered error in post toot. Trying HQ only.")
+        e = sys.exc_info()[0]
+        print("Exception was: " + str(e))
+        
+        try:
+            if userSpec == False:
+                mastodon_api.status_post("seed phrase: " + seedphrase + "(HQ: " + hqLink + " )")
+            else:
+                mastodon_api.status_post("@" + seedphrase + " here is your personal quasicrystal: (HQ: " + hqLink + " )")
+        except:
+             print("HQ only toot failed. Skipping.")
